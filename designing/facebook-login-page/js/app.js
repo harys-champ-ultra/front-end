dd050821b4f2de2d01ad7d6c21c57e59d8181888
harys-champ-ultra <harisamjad56@gmail.com>
2023-09-06 02:08:07 +0500

    new commit

---
+++ designing/facebook-login-page/js/app.js
@@ -0,0 +1,9 @@
+function signUp() {
+    document.getElementById("signin").style.display = "none";
+    document.getElementById("signup").style.display = "block";
+}
+
+function signIn() {
+    document.getElementById("signin").style.display = "block";
+    document.getElementById("signup").style.display = "none";
+}
